dda2005b560e6d03d30ddb10c6db8ab5d5774b53
emmyjarz <emmyjarz@gmail.com>
2017-08-05 22:42:14 -0700

    format code

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,13 @@
 var friendsArr = require("../data/friends");
 
-module.exports = (app) =>{
+module.exports = (app) => {
 
-    
-    app.get("/api/friendsList", (req, res) =>{
+
+    app.get("/api/friendsList", (req, res) => {
         res.json(friendsArr);
     });
 
-    app.post("/api/friendsList", (req, res)=>{
+    app.post("/api/friendsList", (req, res) => {
         friendsArr.push(req.body);
         res.json(friendsArr);
     });
